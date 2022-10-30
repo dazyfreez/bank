f2875274e254f59694fcabe37b5ac91c50e617c1
Christian Gragert <dazyfreez@gmail.com>
2022-10-30 20:05:20 +0100

    x

--- kunde.java
+++ kunde.java
@@ -1,2 +1,35 @@
 public class kunde {
+    private String name;
+    private String adresse;
+    private String telefon;
+    private String email;
+    private String kundennummer;
+    private String kontonummer;
+    private String alter;
+    private String geschlecht;
+    private String geburtsdatum;
+    private String geburtsort;
+    private String geburtsland;
+    private String staatsangehoerigkeit;
+    private String beruf;
+    private String familienstand;
+    private String anzahlKinder;
+    public kunde(String name, String adresse, String telefon, String email, String kundennummer, String kontonummer, String alter, String geschlecht, String geburtsdatum, String geburtsort, String geburtsland, String staatsangehoerigkeit, String beruf, String familienstand, String anzahlKinder) {
+        this.name = name;
+        this.adresse = adresse;
+        this.telefon = telefon;
+        this.email = email;
+        this.kundennummer = kundennummer;
+        this.kontonummer = kontonummer;
+        this.alter = alter;
+        this.geschlecht = geschlecht;
+        this.geburtsdatum = geburtsdatum;
+        this.geburtsort = geburtsort;
+        this.geburtsland = geburtsland;
+        this.staatsangehoerigkeit = staatsangehoerigkeit;
+        this.beruf = beruf;
+        this.familienstand = familienstand;
+        this.anzahlKinder = anzahlKinder;
+    }
+
 }
